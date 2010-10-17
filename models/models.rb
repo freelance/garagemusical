5e455c63236847271f08b22a74dbe3215b60619d
locks <rokusu@gmail.com>
2010-10-17 11:05:00 +0100

    added index to User model and added Posting model

--- models/models.rb
+++ models/models.rb
@@ -4,5 +4,15 @@ class User < Mongomatic::Base
   def validate
     self.errors << ["Name", "can't be empty"] if self["name"].blank?
   end
+  
+  def self.create_indexes
+    collection.create_index('nick', :unique => true)
+  end
+  def self.drop_indexes
+    collection.drop_indexes
+  end
 
+end
+
+class Posting < Mongomatic::Base
 end
